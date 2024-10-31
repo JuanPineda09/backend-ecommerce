396516a07e352a672a7b253dd3b3a008e352d19e
Juan David Pineda Sanchez <jpineda@superricas.com>
2024-10-31 16:47:18 -0500

    ajustes rutas

--- routes/categoriasRoutes.js
+++ routes/categoriasRoutes.js
@@ -1,17 +1,17 @@
 const express = require("express");
 const categoriaControllers = require('../controllers/categoriaController');
-
+const authMiddleware = require('../middleware/authMiddleware');
 
 const router = express.Router();
 
 router.get("/categorias", categoriaControllers.getCategorias);
 
-router.get("/categorias/:idCategoria", categoriaControllers.getIdCategorias);
+router.get("/categorias/:idCategoria", authMiddleware, categoriaControllers.getIdCategorias);
 
 router.post("/categorias", categoriaControllers.postCategorias);
 
-router.delete("/categorias/:idCategoria", categoriaControllers.deleteIdCategorias);
+router.delete("/categorias/:idCategoria", authMiddleware, categoriaControllers.deleteIdCategorias);
 
-router.put("/categorias/:idCategoria", categoriaControllers.putIdCategorias);
+router.put("/categorias/:idCategoria", authMiddleware, categoriaControllers.putIdCategorias);
 
 module.exports = router;
--- routes/productosRoutes.js
+++ routes/productosRoutes.js
@@ -1,17 +1,18 @@
 const express = require("express");
 const productosControllers = require('../controllers/productosController');
+const authMiddleware = require('../controllers/authController}');
 
 
 const router = express.Router();
 
 router.get("/productos", productosControllers.getProductos);
 
-router.get("/productos/:idProducto", productosControllers.getIdProductos);
+router.get("/productos/:idProducto", authMiddleware, productosControllers.getIdProductos);
 
 router.post("/productos", productosControllers.postProductos);
 
-router.delete("/productos/:idProducto", productosControllers.deleteIdProductos);
+router.delete("/productos/:idProducto", authMiddleware, productosControllers.deleteIdProductos);
 
-router.put("/productos/:idProducto", productosControllers.putIdProductos);
+router.put("/productos/:idProducto", authMiddleware, productosControllers.putIdProductos);
 
 module.exports = router;
